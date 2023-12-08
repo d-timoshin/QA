bd2701d07e39bd97c8086bccb0718f733effd164
d-timoshin <153313410+d-timoshin@users.noreply.github.com>
2023-12-08 22:52:36 +0300

    Create 500.py
    py

---
+++ 500.py
@@ -0,0 +1,9 @@
+primes = []
+numbers = list(range(2, 1001))
+
+while numbers:
+    prime = numbers[0]
+    primes.append(prime)
+    numbers = [n for n in numbers if n % prime != 0]
+
+print(primes)
